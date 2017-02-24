474a0f9bd7f2ef4c29a51f2d547e06db36dc3072
muschellij2 <muschellij2@gmail.com>
2017-02-24 14:51:28 -0500

    Added ws_img_data and vignette.

--- R/download_img_data.R
+++ R/download_img_data.R
@@ -2,14 +2,13 @@
 #' @description Download T1 and T2 data for Examples
 #' @return Logical indicator if the files were downloaded.
 #' @param lib.loc a character vector with path names of R libraries. 
-#' Passed to \code{\link{system.file}} 
+#' Passed to \code{\link{img_data}} 
 #' @export
 download_img_data = function(lib.loc = NULL){
   stubs = c("T1Strip.nii.gz",
-            "T2Strip.nii.gz")
-  img_files = system.file(stubs,
-                          package = "WhiteStripe",
-                          lib.loc = lib.loc)
+            "T2Strip.nii.gz")  
+  img_files = ws_img_data(lib.loc = lib.loc,
+                          warn = FALSE)
   
   if (!all(file.exists(img_files))) {
     for (istub in stubs) {
@@ -20,8 +19,31 @@ download_img_data = function(lib.loc = NULL){
       download.file(url, urlfile, quiet = TRUE)
     }
   }
-  img_files = system.file(stubs,
-                          package = "WhiteStripe", 
-                          lib.loc = lib.loc)
+  img_files = ws_img_data(lib.loc = lib.loc,
+                          warn = FALSE)
+
   return(all(file.exists(img_files)))
+}
+
+#' @title Return Filenames of T1 and T2 data
+#' @description Return filenames T1 and T2 data for example 
+#' and vignettes
+#' @return Vector of filenames
+#' @param lib.loc a character vector with path names of R libraries. 
+#' Passed to \code{\link{system.file}} 
+#' @param warn Should a warning be printed if the images were not
+#' there
+#' @export
+ws_img_data = function(lib.loc = NULL, warn = TRUE){
+  stubs = c("T1Strip.nii.gz",
+            "T2Strip.nii.gz")
+  img_files = system.file(stubs,
+                          package = "WhiteStripe",
+                          lib.loc = lib.loc)
+  if (all(img_files == "")) {
+    if (warn) {
+      warning("Files are not downloaded, use download_img_data first!")
+    }
+  }
+  return(img_files)
 }
